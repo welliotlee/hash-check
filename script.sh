a099429dc05dd0d56f23bcf4a8116b41d802bfc9
willilee <willilee@tesla.com>
2019-06-12 03:37:39 -0700

    Creating another file

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ GIT_HASH=$(echo $(git rev-parse HEAD))
 GIT_COMMENT=$(git show -s --format=%B $GIT_HASH)
 CURRENT_TIME=$(date +"[%m/%d/%Y %H:%M:%S]")
 
-printf "%-20s %-15s %-45s %s\n" "$CURRENT_TIME" $USER $GIT_HASH "$GIT_COMMENT" >> sample_log2.txt
+printf "%-20s %-15s %-45s %s\n" "$CURRENT_TIME" $USER $GIT_HASH "$GIT_COMMENT" >> sample_log.txt
